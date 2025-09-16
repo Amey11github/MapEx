c6bf0bb2a49e5da12b1c9f93b5209265e60750c4
Amey Patil <97515279+Amey11github@users.noreply.github.com>
2025-09-16 23:25:32 +0530

    Add files via upload

---
+++ TreeMapEx1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class TreeMapEx1
+{
+	public static void main(String[] args) {
+		Map<Integer,String> map=new TreeMap<>();
+		map.put(1,"hello");
+		map.put(2,"java");
+		map.put(9,"avc");
+		map.put(6,"sdjd");
+		map.put(3,"djghf");
+		map.put(7,"dhjgdf");
+		map.put(11,"dff");
+		map.put(12,"dup");
+		map.put(13,"dfhf");
+		map.put(14,"dfhj");
+		map.put(15,"tyew");
+		map.put(16,"dfhjg");
+		map.put(1,"sdhg");
+		map.put(4,"dgdff");
+		map.put(8,"dhgdf");
+		map.put(21,"sjdkh");
+		map.put(22,"etrey");
+		// map.put(null,"sdhjgd");
+		map.put(33,null);
+		// map.put(null,"sdhjgd1");
+		// map.put(null,"sdhjgd2");
+		map.put(34,"xbcyue");
+
+		System.out.println(map);
+	}
+}
